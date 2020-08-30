3f010065623480e79e10ae489f1f544ebf2a7f64
sudeeprp <sudeep.rp@gmail.com>
2020-08-30 21:39:02 +0530

    stats calculator

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# statisticker
+# Compute Statistics
+
+Here we compute statistics for a bunch of numbers.
+
+## The task
+
+The code is not complete and doesn't even compile.
+Have a look at the tests to know our intention.
+Design the return type accordingly.
+You need to design a test for an error condition as well.
+
+Take care not to leave behind any compiler warnings in your solution.
---
+++ Statistics.Test/StatsUnitTest.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+using Statistics;
+
+namespace Statistics.Test
+{
+    public class StatsUnitTest
+    {
+        [Fact]
+        public void ReportsAverageMinMax()
+        {
+            var statsComputer = new StatsComputer();
+            var computedStats = statsComputer.CalculateStatistics(
+                new List<___>{1.5, 8.9, 3.2, 4.5});
+            float epsilon = 0.001F;
+            Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
+            Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
+            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
+        }
+        public void ReportsNaNForEmptyInput()
+        {
+            var statsComputer = new StatsComputer();
+            var computedStats = statsComputer.CalculateStatistics(
+                new List<___>{});
+            //All fields of computedStats (average, max, min) must be
+            //Double.NaN (not-a-number), as described in
+            //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
+        }
+    }
+}
